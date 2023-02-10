778351e73474e5311db45546b28caa929261cea8
vmse98 <125083996+vmse98@users.noreply.github.com>
2023-02-10 15:57:55 -0300

    script.sh

---
+++ script.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+echo "Criando os diretorios..."
+
+mkdir  /publico
+mkdir  /adm
+mkdir  /ven
+mkdir  /sec
+
+echo "Criando os grupos de usuarios..."
+
+groupadd GRP_ADM
+groupadd GRP_VEN
+groupadd GRP_SEC
+
+echo "Criando os usuarios..."
+
+useradd carlos -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+useradd maria -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+useradd joao -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+
+useradd debora -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+useradd sebastian -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+useradd roberto -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+
+useradd josefina -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+useradd amanda -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+useradd roger -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+
+echo "Especificando permissões dos diretorios..."
+
+chown root:GRP_ADM /adm
+chown root:GRP_VEN /ven 
+chown root:GRP_SEC /sec  
+
+chmod 770 /adm
+chmod 770 /ven
+chmod 770 /sec
+chmod 777 /publico
+
+echo "Fim"
